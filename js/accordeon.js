5a91be7c809f9c9176e4f9873742715a785e159f
KirillBorodulin <Kirill_Borodulin_2005@mail.ru>
2025-05-30 20:13:40 +0500

    work2

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,16 +1,24 @@
-const accordeonQustion = document.querySelector(".accordeon__qustion");
+const accordeonQuestions = document.querySelectorAll('.accordeon__question');
 
-accordeonQustion.forEach(function (accordeonQustion) {
-  accordeonQustion.addEventListener("click", function () {
-    const clickedAnswer = accordeonQustion.parentElement.querySelector(".accordeon__answer");
+accordeonQuestions.forEach(function (accordeonQuestion) {
+    accordeonQuestion.addEventListener("click", function () {
+        const clickedAnswer = accordeonQuestion.nextElementSibling;
 
-    accordeonQustion.classList.toggle('.accordeon__qustion--active');
-    clickedAnswer.classList.toggle('.accordeon__answer--visible');
+        // Устанавливаем/удаляем класс active
+        accordeonQuestion.classList.toggle('accordeon__question--active');
+        clickedAnswer.classList.toggle('accordeon__answer--visible');
 
-    if (clickedAnswer.classList.contains('.accordeon__answer--visible')) {
-      clickedAnswer.style.maxHeight = clickedAnswer.scrollHeight + "px";
-    } else {
-      clickedAnswer.style.maxHeight = null;
-    }
-  });
+        // Проверяем, содержит ли clickedAnswer класс accordeon_answer--visible
+        if (clickedAnswer.classList.contains('accordeon__answer--visible')) {
+            clickedAnswer.style.maxHeight = clickedAnswer.scrollHeight + "px";
+        } else {
+            clickedAnswer.style.maxHeight = null;
+        }
+    });
 });
+
+const firstAnswer = accordeonQuestions[0].nextElementSibling;
+accordeonQuestions[0].classList.add('accordeon_question--active');
+firstAnswer.classList.add('accordeon_answer--visible');
+firstAnswer.style.maxHeight = firstAnswer.scrollHeight + "px";
+
